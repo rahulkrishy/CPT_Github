8ce47c08eef4bcf2e9c9a5ddb98f8e2808a68bc0
rahulkrishy <rahulkrishy@gmail.com>
2024-02-20 17:32:48 +0530

    second  commit

--- code_1.py
+++ code_1.py
@@ -1,3 +1,5 @@
 # This code print "Hello World"
-
 print("Hello World")
+
+# This code print "welcome to rahulkrishy github first repository"
+print("welcome to rahulkrishy github first repository")
