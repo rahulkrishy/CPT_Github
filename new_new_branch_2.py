0933eb1f5f0e0c1dc30c8067ac9a7e7ee25c0d1a
rahulkrishy <rahulkrishy@gmail.com>
2024-02-21 19:11:56 +0530

    added message 1 new_new_branch_2.py

--- new_new_branch_2.py
+++ new_new_branch_2.py
@@ -1,9 +1,3 @@
 # file to check revert and reset feat in git 
 
 print("first message from new_new_branch_2.py file") # commited here (1)
-
-print("Second message from new_new_branch_2.py file") # commited here (2)
-
-print("3rd message from new_new_branch_2.py file") # commited here (3)
-
-print("fourth message from new_new_branch_2.py file") # commited here (4)
