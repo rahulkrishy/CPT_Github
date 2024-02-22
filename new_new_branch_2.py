3bf75941a910f229750cf1933930d6a8ed7452e0
rahulkrishy <rahulkrishy@gmail.com>
2024-02-22 09:46:11 +0530

    1 commit

--- new_new_branch_2.py
+++ new_new_branch_2.py
@@ -2,6 +2,3 @@
 
 print("first message from new_new_branch_2.py file") # commited here (1)
 
-print("Second message from new_new_branch_2.py file") # commited here (2)
-
-print("third message from new_new_branch_2.py file") # commited here (3)
